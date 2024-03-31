872496bb00768be2e28138cf6a50a6e4fb8e6f9c
dreamanlan <dreaman_163@163.com>
2024-03-31 17:08:04 +0800

    安卓编译

---
+++ make_android.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export ANDROID_SDK=/c/sdk_full
+export ANDROID_NDK=/e/android-ndk-r19c
+export JAVA_HOME=/e/jdk-1.8
+
+export APK_TARGET_ID=android-28
+export ANDROID_SDK_ROOT_PATH=%ANDROID_SDK%
+export ANDROID_NDK_ROOT_PATH=%ANDROID_NDK%
+
+mkdir -p build-android-v8a
+cd build-android-v8a
+cmake -DBUILD_ANDROID=On -DANDROID_ABI=arm64-v8a -G "MinGW Makefiles" ..
+/E/android-ndk-r19c/prebuilt/windows-x86_64/bin/make.exe VERBOSE=1
+
+cd ..
+mkdir -p build-android-v7a
+cd build-android-v7a
+cmake -DBUILD_ANDROID=On -DANDROID_ABI=armeabi-v7a -G "MinGW Makefiles" ..
+/E/android-ndk-r19c/prebuilt/windows-x86_64/bin/make.exe VERBOSE=1

4ac8b22ac41d7703d88bd26d85d567a53d4e532e
dreamanlan <dreaman_163@163.com>
2024-04-02 21:11:07 +0800

    driver拷贝与签名脚本

--- copyandsign.bat
+++ copyandsign.bat
@@ -2,9 +2,9 @@
 rem working directory
 
 set workdir=%~dp0
-cd %workdir%
+cd "%workdir%"
 
-if not exist "C:\Program Files\RenderDoc\plugins\android\sign.bat" copy sign.bat "C:\Program Files\RenderDoc\plugins\android\sign.bat"
+copy /y sign.bat "C:\Program Files\RenderDoc\plugins\android\sign.bat"
 
 copy /y build-android-v8a\bin\org.renderdoc.renderdoccmd.arm64.apk "C:\Program Files\RenderDoc\plugins\android\"
 copy /y build-android-v7a\bin\org.renderdoc.renderdoccmd.arm32.apk "C:\Program Files\RenderDoc\plugins\android\"
